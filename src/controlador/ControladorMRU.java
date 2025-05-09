f095b4bac78b3c89361a28b08350f83384972ed9
Isaac Ventura <eventurag3@miumg.edu.gt>
2025-05-09 10:31:29 -0600

    Metodos para calculo de distancia y velocidad

--- src/controlador/ControladorMRU.java
+++ src/controlador/ControladorMRU.java
@@ -25,6 +25,20 @@ public class ControladorMRU implements ActionListener {
      double tiempo = distancia/velocidad;
      modeloMRU.getVistaMRU().lblResultadoTiempo.setText(tiempo + " Segundos");
  }
+ 
+ public void calculoDistancia(){
+     double velocidad = Double.parseDouble(modeloMRU.getVistaMRU().txtVelocidadD.getText());
+     double tiempo = Double.parseDouble(modeloMRU.getVistaMRU().txtTiempoD.getText());
+     double distancia = velocidad*tiempo;
+     modeloMRU.getVistaMRU().lblResultadoDIstancia.setText(distancia + " Metros");
+ }
+ 
+ public void calculoVelocidad(){
+     double distancia = Double.parseDouble(modeloMRU.getVistaMRU().txtDistanciaV.getText());
+     double tiempo = Double.parseDouble(modeloMRU.getVistaMRU().txtTiempoV.getText());
+     double velocidad = distancia/tiempo;
+     modeloMRU.getVistaMRU().lblResultadoVelocidad.setText(velocidad + "Metros/Segundos");
+ }
     
    
     
