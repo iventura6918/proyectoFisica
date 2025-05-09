61afe393115471dc43c518ea2d586d62c9c9e9b4
Isaac Ventura <eventurag3@miumg.edu.gt>
2025-05-09 10:51:08 -0600

    Boton para la vista del MRU

--- src/controlador/ControladorMenuprincipal.java
+++ src/controlador/ControladorMenuprincipal.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import modelo.ModeloMenuPrincipal;
 import vista.VIstaConversiones;
+import vista.VIstaMRU;
 import vista.VistaCalculoHipotenusa;
 import vista.VistaSumaVectores;
 
@@ -29,6 +30,9 @@ public class ControladorMenuprincipal implements ActionListener{
         if (e.getActionCommand().equals(modelomp.getVistamp().bntConversiones.getActionCommand())) {
             callVistaConversiones();
         }
+        if (e.getActionCommand().equals(modelomp.getVistamp().btnMRU.getActionCommand())) {
+            callVistaMRU();
+        }
     }
     
     public void callVistaSumaVector(){
@@ -44,6 +48,11 @@ public class ControladorMenuprincipal implements ActionListener{
     public void callVistaConversiones(){
         VIstaConversiones vistaCon = new VIstaConversiones();
         vistaCon.setVisible(true);
+        modelomp.getVistamp().dispose();
+    }
+    public void callVistaMRU(){
+        VIstaMRU vistaMRU = new VIstaMRU();
+        vistaMRU.setVisible(true);
         modelomp.getVistamp().dispose();
     }
     
